ee77e9ee3c735e4900403038c4303afeac3135ce
kuran_kuran <kuran_kuran@ka2.so-net.ne.jp>
2021-07-25 16:32:00 +0900

    最初のバージョン

---
+++ Assets/Script/Purchase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class Purchase : MonoBehaviour
+{
+    private class Item
+    {
+        public string title;
+        public string description;
+        public string price;
+    };
+
+    private void Start()
+    {
+        IStoreController controller = CodelessIAPStoreListener.Instance.StoreController;
+        List<Item> items = new List<Item>();
+        foreach(Product product in controller.products.all)
+        {
+            items.Add(new Item
+            {
+                title = product.metadata.localizedTitle,
+                description = product.metadata.localizedDescription,
+                price = product.metadata.localizedPriceString
+            });
+        }
+    }
+
+    private void Update()
+    {
+    }
+
+    // 購入成功
+    public void OnPurchaseComplete(Product product)
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorUtility.DisplayDialog("Purchase complete", product.definition.id, "OK");
+#endif
+    }
+
+    // 購入失敗
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason reason)
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorUtility.DisplayDialog("Purchase failed", product.definition.id, "OK");
+#endif
+    }
+}
